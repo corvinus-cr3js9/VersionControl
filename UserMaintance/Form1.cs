73492d0a666afdcda4b7aac0d6b4dcc9796e1bcd
Dominik Sádt <dominik.sadt@stud.uni-corvinus.hu>
2021-10-01 17:51:15 +0200

    Utónév

--- UserMaintance/Form1.cs
+++ UserMaintance/Form1.cs
@@ -15,7 +15,7 @@ namespace UserMaintance
     public partial class Form1 : Form
     {
         BindingList<User> users = new BindingList<User>();
-
+        
         public Form1()
         {
             InitializeComponent();
