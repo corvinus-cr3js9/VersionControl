caef20a1a69f59a1fd193579f622e83879c0f1bf
Dominik Sádt <dominik.sadt@stud.uni-corvinus.hu>
2021-10-12 16:28:24 +0200

    2. task

--- Olympic_mintazh v2/Form1.cs
+++ Olympic_mintazh v2/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,36 @@ namespace Olympic_mintazh_v2
 {
     public partial class Form1 : Form
     {
+        List<OlympicResult> results = new List<OlympicResult>();
+
         public Form1()
         {
             InitializeComponent();
+            Load("Summer_olympic_Medals.csv");
+        }
+        private void Load(string Filename)
+        {
+            using (var sr = new StreamReader(Filename, Encoding.Default))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(',');
+                    var or = new OlympicResult()
+                    {
+                        Year = int.Parse(line[0]),
+                        Country = line[3],
+                        Medals = new int[]
+                        {
+                            int.Parse(line[5]),
+                            int.Parse(line[6]),
+                            int.Parse(line[7])
+                        }
+
+
+                    };
+                }
+            }
         }
     }
 }
