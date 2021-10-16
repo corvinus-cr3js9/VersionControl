4397c9ae3224a42cbe347e3bc1dacd185a1adaed
Dominik Sádt <dominik.sadt@stud.uni-corvinus.hu>
2021-10-16 15:27:36 +0200

    datagridview2

---
+++ weks05/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weks05.Entities
+{
+    class PortfolioItem
+    {
+        public string Index { get; set; }
+        public decimal Voluem { get; set; }
+    }
+}
--- weks05/Form1.cs
+++ weks05/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using weks05.Entities;
 
 namespace weks05
 {
@@ -14,12 +15,25 @@ namespace weks05
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
+            CreatePortfolio();
 
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+            
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Voluem = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Voluem = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Voluem = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
