f6843d34e45e0d49fc20c1721e51e24a72c1eed0
Dominik Sádt <dominik.sadt@stud.uni-corvinus.hu>
2021-11-23 15:39:40 +0100

    Creating AccountControllerTestFixture

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test] // Attribútum
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+
+        }
+    }
+}
