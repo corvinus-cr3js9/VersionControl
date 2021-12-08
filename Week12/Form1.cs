2e372e8ed3a2b9b6c9dba6c5a8f86a7c7daae6ba
Dominik Sádt <dominik.sadt@stud.uni-corvinus.hu>
2021-12-08 19:36:17 +0100

    jatekter

--- Week12/Form1.cs
+++ Week12/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WorldsHardestGame;
 
 namespace Week12
 {
     public partial class Form1 : Form
     {
+        public GameController gc= new GameController();
+        GameArea ga;
+           
+        
         public Form1()
         {
             InitializeComponent();
+            ga = gc.ActivateDisplay();
+            this.Controls.Add(ga);
         }
     }
 }
